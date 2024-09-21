f4efca9dba48ae49b790838f9740b01bb347517a
Nicola <nicolahu1997@gmail.com>
2024-09-21 13:38:51 +0200

    new ex map

---
+++ 21_09_2024/concepts/index.js
@@ -0,0 +1,22 @@
+
+
+// HIGH ORDER FUNCTIONS
+// Map это метод который используется на списках. 
+function Add2(x) {
+    return x + 2
+}
+
+let result = [1, 2, 3, 4, 5].map(Add2)
+
+console.log(result)  // [3,4,5,6,7]
+
+
+// Ex. 2
+// Everyone below is birth the 1st of January.
+let yearOfBirth = [2001, 2002, 1999, 2000]
+
+// With the help of map function, return their actual ages: // result: // [23, 22, 25, 24]
+
+// SOL: // решайте 
+
+
