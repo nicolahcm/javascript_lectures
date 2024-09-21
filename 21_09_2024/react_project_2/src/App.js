215c7b44a88f94c573a6373d3cf1e3adc25e117e
Nicola <nicolahu1997@gmail.com>
2024-09-21 14:03:29 +0200

    added button react

---
+++ 21_09_2024/react_project_2/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Button from './components/Button';
+
+export default function App() {
+
+  return <div>
+    Hello World
+
+    This is a Button:
+    <Button>
+
+    </Button>
+  </div>
+}
