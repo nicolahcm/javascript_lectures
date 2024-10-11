d8fb4a84e764a3ec779976cab5837050af9c358f
Nicola <nicolahu1997@gmail.com>
2024-10-11 10:26:56 +0200

    intermediate

--- 5_10_2024/card_change/src/App.js
+++ 5_10_2024/card_change/src/App.js
@@ -1,71 +1,43 @@
 import React, { useState } from 'react';
 
-const FormCreateCard = ({ bodyCard, setBodyCard, changeView }) => {
-
-  const handleChangeBody = (e) => {
-    setBodyCard(e.target.value)
-  }
-
-  const handleClickBtnAdd = (e) => {
-    e.preventDefault()
-    changeView(e)
-  }
-
+const FormCreateCard = (props) => {
   return (
-    <div className="containerOfNewCardBtn">
-      <div className="formWrapper">
-        <form >
-          <div className="mb-3">
-            <label className="form-label">Content</label>
-            <textarea rows="4" type="text" className="form-control input-group-sm" placeholder="Insert the content of the card" value={bodyCard} onChange={handleChangeBody} />
-          </div>
-
-          <div className="buttonsContainerForm">
-            <button className="btn btn-primary" onClick={handleClickBtnAdd}>Add</button>
-            <button className="btn btn-primary" onClick={changeView}>Back</button>
-          </div>
-
-        </form>
-      </div>
+    <div>
+      <form>
+        <label className="form-label">Content</label>
+        <textarea rows="7" type="text" className="form-control input-group-sm" placeholder="Insert the content of the card" />
+        <button>Add</button>
+        <button onClick={props.helloWorld}>Back</button>
+      </form>
     </div>
   )
 }
 
-
-const Card = ({ body, changeView }) => {
-
-  return (
-    <div className="card text-white bg-primary mb-3 singleCardForArr">
-      <div>{body}</div>
-      <button className="btn btn-primary" onClick={changeView}>Edit</button>
-    </div >
-  )
-
+const Card = (props) => {
+  return <div>
+    This is a card
+    <button onClick={props.helloWorld}>Back</button>
+  </div>
 }
 
 function App() {
-
-  const [bodyCard, setBodyCard] = useState("")
-  const [cardOrEditForm, setCardOrEditForm] = useState("C")
-
-
-  const changeView = (e) => {
+  const [cardOrForm, setCardOrForm] = useState("F")
+  const helloWorld = (e) => {
     e.preventDefault()
-    let newValue = cardOrEditForm == "C" ? "F" : "C"
-    console.log(newValue)
-    setCardOrEditForm(newValue)
+    console.log("Hello World")
+    let newValue = cardOrForm == "C" ? "F" : "C"
+    setCardOrForm(newValue)
   }
 
-  if (cardOrEditForm == "C") {
-    return (
-      <div className="App">
-        <FormCreateCard setBodyCard={setBodyCard} bodyCard={bodyCard} changeView={changeView} />
-      </div>
-    );
-  } else if (cardOrEditForm == "F") {
-    return <div className="App">
-      <Card body={bodyCard} changeView={changeView}></Card>
+  if (cardOrForm == "F") {
+    return (<div className="App">
+      <FormCreateCard helloWorld={helloWorld} />
     </div>
+    )
+  } else {
+    return (<div className="App">
+      <Card helloWorld={helloWorld}></Card>
+    </div>)
   }
 }
 
